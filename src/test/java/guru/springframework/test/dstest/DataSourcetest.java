665af0afc92f88778311ec953e75ebe7dbe59b43
Kostiuk_N <kost.nik88@gmail.com>
2019-02-24 19:29:51 +0200

    @profile, @ActiveProfile for tests

---
+++ src/test/java/guru/springframework/test/dstest/DataSourcetest.java
@@ -0,0 +1,35 @@
+package guru.springframework.test.dstest;
+
+
+import guru.springframework.test.config.DataSourceConfig;
+import guru.springframework.test.ds.FakeDataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(DataSourceConfig.class)
+//if none than "default"
+//@ActiveProfiles("qa")
+public class DataSourcetest {
+
+    private FakeDataSource fakeDataSource;
+
+
+    @Autowired
+    public void setFakeDataSource(FakeDataSource fakeDataSource) {
+        this.fakeDataSource = fakeDataSource;
+    }
+
+
+    @Test
+    public void testDatasource() {
+        System.out.println(fakeDataSource.getConnectionInfo());
+        System.out.println(fakeDataSource.toString());
+    }
+
+
+}
