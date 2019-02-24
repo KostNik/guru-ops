b362b41d3841f47049650cea7e11db8694cf8cce
Kostiuk_N <kost.nik88@gmail.com>
2019-02-24 18:32:16 +0200

    properties

---
+++ src/test/java/guru/test/config/external/props/ExternalPropsMultiFile.java
@@ -0,0 +1,30 @@
+package guru.test.config.external.props;
+
+import guru.springframework.test.jms.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+/**
+ * since Spring 3.1
+ * Created by jt on 5/7/16.
+ */
+@Configuration
+@PropertySource({"classpath:testing.properties", "classpath:encrypted-testing.properties"})
+public class ExternalPropsMultiFile {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public FakeJmsBroker fakeJmsBrokerEnv() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUrl(environment.getProperty("guru.jms.server"));
+        fakeJmsBroker.setPort(environment.getRequiredProperty("guru.jms.port", Integer.class));
+        fakeJmsBroker.setUser(environment.getProperty("guru.jms.user"));
+        fakeJmsBroker.setPassword(environment.getProperty("guru.jms.encrypted.password"));
+        return fakeJmsBroker;
+    }
+}
